5f7cfea71f8453097eac59573fe91083eed71f7d
Vivian Cheng <viviancheng@Vivians-MacBook-Air.local>
2021-02-05 16:10:47 -0500

    separated header

--- ewb2018/website2018/gallery.php
+++ ewb2018/website2018/gallery.php
@@ -1,26 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>MIT Engineers Without Borders</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/modern-business.css" rel="stylesheet">
-
-    <!-- Google font -->
-    <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
-    <!-- Website created by Vibha Agarwal (agarwalv@mit.edu) for MIT EWB 2018.  -->
-
-</head>
+<?php include("header.php"); ?>
 
 <body>
 
--- ewb2018/website2018/moringa.php
+++ ewb2018/website2018/moringa.php
@@ -1,26 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>MIT Engineers Without Borders</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/modern-business.css" rel="stylesheet">
-
-    <!-- Google font -->
-    <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
-    <!-- Website created by Vibha Agarwal (agarwalv@mit.edu) for MIT EWB 2018.  -->
-
-  </head>
+  <?php include("header.php"); ?>
 
   <body>
 
@@ -38,7 +19,7 @@
 
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
-          <a href="index.php">Projects</a>
+          <a href="projects.php">Projects</a>
         </li>
         <li class="breadcrumb-item active">Moninga Connect</li>
       </ol>
