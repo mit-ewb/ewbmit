ac81c9905e3961823a18b5d5e625a101e09f6fb3
Vivian Cheng <viviancheng@Vivians-MacBook-Air.local>
2021-06-30 14:30:17 -0400

    temp test embed

---
+++ ewb2018/website2018/test.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>UIL Embed Test</title>
+	<link rel="stylesheet" href="./css/test.css"
+</head>
+
+<body>
+
+
+<!-- Page Content -->
+<div class="container">
+
+	<div>
+		<iframe src="https://axishelps.org"></iframe>
+		<!-- Page Heading/Breadcrumbs -->
+		<h1 class="mt-4 mb-3">Updates
+			<small>Events</small>
+		</h1>
+        <iframe src="https://calendar.google.com/calendar/embed?src=p0ohh9gb2t43cs2hlp0d11v77o%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+<!--		<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23aff7f1&amp;ctz=America%2FNew_York&amp;src=dml2aWFuY2hlbmcyNjdAZ21haWwuY29t&amp;src=dml2aWFuQHVyYmFuaW1wYWN0bGFiLmNvbQ&amp;color=%23bab2cc&amp;color=%23009688&amp;showTitle=0&amp;showPrint=0&amp;showCalendars=1&amp;showTz=1&amp;showDate=1&amp;showNav=1&amp;showTabs=1" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>-->
+	</div>
+
+
+</div>
+
+<!-- /.container -->
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
